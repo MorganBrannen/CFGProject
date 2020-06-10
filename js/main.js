6f3cb93ec362f6413f334ef517bb444218b137a6
MorganBrannen <64012304+MorganBrannen@users.noreply.github.com>
2020-06-10 16:54:18 +0100

    button functions

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ function sayThanks() {
     return false;
 }
 
+var mybutton = document.getElementbyId("myBtn");
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
